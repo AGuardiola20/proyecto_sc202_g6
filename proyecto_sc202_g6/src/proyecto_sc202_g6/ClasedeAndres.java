274ceb922934d3e99aab24ac5111d40b4a7e8c05
AGuardiola20 <andres.pguardiola@gmail.com>
2025-06-15 21:40:35 -0600

    nueva class

---
+++ proyecto_sc202_g6/src/proyecto_sc202_g6/ClasedeAndres.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_sc202_g6;
+
+/**
+ *
+ * @author Andres
+ */
+public class ClasedeAndres {
+    
+}
